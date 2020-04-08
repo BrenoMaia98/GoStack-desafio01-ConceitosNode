72c91701c858f65c18a54ab496d936a77f791937
BrenoMaia98 <brenomaia98@gmail.com>
2020-04-08 00:04:00 -0300

    Rota inserção de repositorios finalizada

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
-const { middlewareLogger , returnLog } = require("./middleware/logger");
-// const { uuid } = require("uuidv4");
+const { middlewareLogger, returnLog } = require("./middleware/logger");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -24,7 +24,35 @@ app.get("/repositories", (request, response) => {
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+  let errors = []
+  let newRepo = {
+    id: '',
+    title,
+    url,
+    techs,
+  }
+
+  if (!newRepo.title)
+    errors.push("Missing body parameter : title");
+
+  if (!newRepo.url)
+    errors.push("Missing body parameter : url");
+
+  if (!newRepo.techs)
+    errors.push("Missing body parameter : techs");
+  else {
+    const hasEmptyValue = newRepo.techs.filter(repo => repo === '');
+    if (hasEmptyValue.length > 0)
+      errors.push("Tech name cannot be empty")
+  }
+
+  if (errors.length > 0) {
+    return response.status(400).json(errors)
+  }
+
+  newRepo.id = uuid();
+  return response.status(200).json(newRepo);
 });
 
 app.put("/repositories/:id", (request, response) => {
